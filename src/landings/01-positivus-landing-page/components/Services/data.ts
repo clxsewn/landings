0a8ad5d0f7884c693f397e9de5811d6ba3c2c843
Alex <apelich77@gmail.com>
2023-11-29 23:14:05 +0200

    landing update

--- src/landings/01-positivus-landing-page/components/Services/data.ts
+++ src/landings/01-positivus-landing-page/components/Services/data.ts
@@ -1,40 +1,46 @@
-import { IServicesItemProps } from '../../components/ServicesItem/SevicesItem.tsx'
+import { IServicesItemProps } from '../../components/Services/SevicesItem.tsx'
+import img1 from '../../assets/services/01.png'
+import img2 from '../../assets/services/02.png'
+import img3 from '../../assets/services/03.png'
+import img4 from '../../assets/services/04.png'
+import img5 from '../../assets/services/05.png'
+import img6 from '../../assets/services/06.png'
 
 export const servicesList: IServicesItemProps[] = [
     {
         type: 'gray',
         mainTextBg: 'green',
         text: 'Search engine optimization',
-        imgSrc: '',
+        imgSrc: img1,
     },
     {
         type: 'green',
-        mainTextBg: 'light',
+        mainTextBg: 'white',
         text: 'Pay-per-click advertising',
-        imgSrc: '',
+        imgSrc: img2,
     },
     {
         type: 'dark',
-        mainTextBg: 'light',
+        mainTextBg: 'white',
         text: 'Social Media Marketing',
-        imgSrc: '',
+        imgSrc: img3,
     },
     {
         type: 'gray',
         mainTextBg: 'green',
         text: 'Email Marketing',
-        imgSrc: '',
+        imgSrc: img4,
     },
     {
         type: 'green',
-        mainTextBg: 'light',
+        mainTextBg: 'white',
         text: 'Content Creation',
-        imgSrc: '',
+        imgSrc: img5,
     },
     {
         type: 'dark',
         mainTextBg: 'green',
         text: 'Analytics and Tracking',
-        imgSrc: '',
+        imgSrc: img6,
     },
 ]
