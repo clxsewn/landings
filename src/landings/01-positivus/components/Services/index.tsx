94410d6ce42e51dc6eb14e8dad11a662308e26b5
Alex <apelich77@gmail.com>
2024-01-10 23:06:50 +0200

    landing update

--- src/landings/01-positivus/components/Services/index.tsx
+++ src/landings/01-positivus/components/Services/index.tsx
@@ -1,10 +1,12 @@
-import './styles.scss'
+import styles from './styles.module.scss'
 import ServicesItem from './SevicesItem.tsx'
 import { servicesList } from './data.ts'
 
 export default function Services() {
+    console.log(styles)
+
     return (
-        <div id="services-list">
+        <div className={styles.servicesList}>
             {servicesList.map((s) => (
                 <ServicesItem key={s.text} {...s} />
             ))}
