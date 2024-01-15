1fa8b7d93a777548b5bc213c71b564d4dd03f38c
Alex <apelich77@gmail.com>
2024-01-15 23:37:00 +0200

    landing update

--- src/landings/02-prohouse/index.tsx
+++ src/landings/02-prohouse/index.tsx
@@ -1,9 +1,15 @@
+import logo from './assets/logo.svg'
+import './styles/main.scss'
+import Logo from '@/02-prohouse/components/Logo.tsx'
+
 export default function Landing2() {
     return (
         <>
             <header className="container">
                 <div className="header__left">
-                    <div className="header__logo"></div>
+                    <div className="header__logo">
+                        <Logo />
+                    </div>
                     <nav>111</nav>
                 </div>
                 <div className="header__right"></div>
